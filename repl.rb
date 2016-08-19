d4653a4cc2fbc9b1c604b4b496c8f19dc3771531
blakeyoder <yoderblake@gmail.com>
2016-08-19 17:09:58 -0400

    Removing gem in place of ligher weight methods

--- repl.rb
+++ repl.rb
@@ -1,18 +1,13 @@
-require 'remedy'
-include Remedy
+require 'io/console'
 
 puts "Ruby REPL"
 
-user_input = Interaction.new
 # Handle the input, this would probably run some method
 # as a part of the DSL you'd have to create. Place this
 # repl as your command line interface to your service.
 def handle_input(input)
   result = eval(input)
   puts(" => #{result}")
-end
-
-def is_up_arrow?
 end
 
 # This is a lambda that runs the content of the block
@@ -27,4 +22,28 @@ end
 # exit or a HUP.
 loop do
   repl[">> "]
+end
+
+# Reads keypresses from the user including 2 and 3 escape character sequences.
+def read_char
+
+  STDIN.echo = false
+  STDIN.raw!
+
+  input = STDIN.getc.chr
+  if input == "\e" then
+    input << STDIN.read_nonblock(3) rescue nil
+    input << STDIN.read_nonblock(2) rescue nil
+  end
+
+  ensure
+  STDIN.cooked!
+  
+  return input
+
+end
+
+def show_single_key
+  c = read_char
+  return c.inspect
 end
